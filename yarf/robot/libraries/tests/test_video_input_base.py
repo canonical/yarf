2e701277b25e9ce39509d82f5aa53bf2f122a8e1
Paolo Gentili <paolo.gentili@canonical.com>
2024-08-19 16:20:22 +0200

    [New] Video Input interface and Zapper implementation (#16)
    * Add: video input library
    
    * Add: tests for the video_input_base module
    
    * Add: tests for the Zapper video input class
    
    * Update yarf/robot/libraries/video_input_base.py
    
    Co-authored-by: Douglas Chiang <39449881+douglasdotc@users.noreply.github.com>
    
    * Update yarf/robot/libraries/video_input_base.py
    
    Co-authored-by: Douglas Chiang <39449881+douglasdotc@users.noreply.github.com>
    
    * Fix: install dependency to run our tests
    
    * Fix: removed working directory
    
    ---------
    
    Co-authored-by: Douglas Chiang <39449881+douglasdotc@users.noreply.github.com>

---
+++ yarf/robot/libraries/tests/test_video_input_base.py
@@ -0,0 +1,236 @@
+"""
+This module provides tests for the Video Input base module.
+"""
+
+import unittest
+from unittest.mock import Mock, patch
+
+from RPA.recognition.templates import ImageNotFoundError
+from yarf.robot.libraries.video_input_base import VideoInputBase
+
+
+class VideoInputBaseTests(unittest.TestCase):
+    """
+    This class provides tests for the VideoInputBase class
+    """
+
+    class VideoInput(VideoInputBase):
+        """
+        Test class with basic implementation for abstract
+        methods.
+        """
+
+        screenshot = Mock()
+
+        def start_video_input(self):
+            pass
+
+        def stop_video_input(self):
+            pass
+
+        def _grab_screenshot(self):
+            return self.screenshot
+
+    @patch("time.time")
+    @patch("yarf.robot.libraries.video_input_base.to_image")
+    def test_match(self, mock_to_image, mock_time):
+        """Check the *Match* keyword returns the regions found."""
+
+        video_input = self.VideoInput()
+        video_input._rpa_images = Mock()
+        video_input._grab_screenshot = Mock()
+        video_input._grab_screenshot.side_effect = [RuntimeError, None]
+
+        mock_time.return_value = 0
+        mock_to_image.return_value = Mock()
+        mock_regions = [Mock()]
+
+        video_input._rpa_images.find_template_in_image.return_value = (
+            mock_regions
+        )
+        self.assertListEqual(
+            video_input.match("path"),
+            [
+                {
+                    "left": mock_regions[0].left,
+                    "top": mock_regions[0].top,
+                    "right": mock_regions[0].right,
+                    "bottom": mock_regions[0].bottom,
+                    "path": "path",
+                }
+            ],
+        )
+
+    @patch("time.time")
+    @patch("yarf.robot.libraries.video_input_base.to_image")
+    def test_match_any(self, mock_to_image, mock_time):
+        """
+        Check the *Match Any* keyword returns the regions found in the first
+        matched template
+        """
+
+        video_input = self.VideoInput()
+        video_input._rpa_images = Mock()
+
+        mock_time.return_value = 0
+        mock_to_image.return_value = Mock()
+        mock_regions = [Mock()]
+        video_input._rpa_images.find_template_in_image.return_value = (
+            mock_regions
+        )
+        self.assertListEqual(
+            video_input.match_any(["path1", "path2"]),
+            [
+                {
+                    "left": mock_regions[0].left,
+                    "top": mock_regions[0].top,
+                    "right": mock_regions[0].right,
+                    "bottom": mock_regions[0].bottom,
+                    "path": "path1",
+                }
+            ],
+        )
+
+    @patch("time.time")
+    @patch("yarf.robot.libraries.video_input_base.to_image")
+    def test_match_all(self, mock_to_image, mock_time):
+        """
+        Check the *Match All* keyword returns the regions found only
+        when every template matches.
+        """
+
+        video_input = self.VideoInput()
+        video_input._rpa_images = Mock()
+
+        mock_time.return_value = 0
+        mock_to_image.return_value = Mock()
+        mock_regions = [Mock()]
+        video_input._rpa_images.find_template_in_image.side_effect = [
+            # At first attempt the second template doesn't match
+            mock_regions,
+            ValueError,
+            # At second attempt every template matches
+            mock_regions,
+            mock_regions,
+        ]
+        self.assertListEqual(
+            video_input.match_all(["path1", "path2"]),
+            [
+                {
+                    "left": mock_regions[0].left,
+                    "top": mock_regions[0].top,
+                    "right": mock_regions[0].right,
+                    "bottom": mock_regions[0].bottom,
+                    "path": "path1",
+                },
+                {
+                    "left": mock_regions[0].left,
+                    "top": mock_regions[0].top,
+                    "right": mock_regions[0].right,
+                    "bottom": mock_regions[0].bottom,
+                    "path": "path2",
+                },
+            ],
+        )
+
+    @patch("time.time")
+    @patch("yarf.robot.libraries.video_input_base.to_image", Mock())
+    def test_match_fail(self, mock_time):
+        """Check the function returns correctly unless there's no match."""
+
+        video_input = self.VideoInput()
+        video_input._rpa_images = Mock()
+        video_input._log_failed_match = Mock()
+
+        # Screenshot not valid
+        video_input._rpa_images.find_template_in_image.side_effect = ValueError
+        mock_time.side_effect = [0, 0, 2]
+
+        with self.assertRaises(ImageNotFoundError):
+            video_input.match("path", timeout=1)
+        video_input._log_failed_match.assert_called_with(
+            video_input.screenshot, "path"
+        )
+
+        # Template not found
+        video_input._rpa_images.find_template_in_image.side_effect = (
+            ImageNotFoundError
+        )
+        mock_time.side_effect = [0, 0, 2]
+
+        with self.assertRaises(ImageNotFoundError):
+            video_input.match("path", timeout=1)
+
+    @patch("time.time")
+    @patch("yarf.robot.libraries.video_input_base.to_image")
+    def test_match_converts_to_rgb(self, mock_to_image, mock_time):
+        """
+        Check the Match keyword accepts non-RGB images and converts them
+        """
+        video_input = self.VideoInput()
+        video_input._rpa_images = Mock()
+
+        mock_time.return_value = 0
+        mock_to_image.return_value = Mock()
+        mock_to_image.return_value.mode = "L"
+        mock_regions = [Mock()]
+        video_input._rpa_images.find_template_in_image.return_value = (
+            mock_regions
+        )
+        video_input.match("path")
+        mock_to_image.return_value.convert.assert_called_with("RGB")
+
+    @patch("yarf.robot.libraries.video_input_base.ocr")
+    def test_read_text(self, mock_ocr):
+        """
+        Test whether the function grabs a new screenshot and runs OCR on it.
+        """
+        video_input = self.VideoInput()
+
+        video_input.read_text()
+
+        mock_ocr.read.assert_called_once_with(video_input.screenshot)
+
+    @patch("yarf.robot.libraries.video_input_base.ocr")
+    def test_read_text_image(self, mock_ocr):
+        """Test whether the function runs OCR on the provided image."""
+
+        video_input = self.VideoInput()
+        image = Mock()
+
+        video_input.read_text(image)
+        mock_ocr.read.assert_called_once_with(image)
+
+    def test_restart_video_input(self):
+        """
+        Test the restart video function calls the inner
+        relative functions.
+        """
+
+        video_input = self.VideoInput()
+        video_input.start_video_input = Mock()
+        video_input.stop_video_input = Mock()
+
+        video_input.restart_video_input()
+
+        video_input.stop_video_input.assert_called_once()
+        video_input.start_video_input.assert_called_once()
+
+    @patch("yarf.robot.libraries.video_input_base.logger")
+    @patch("yarf.robot.libraries.video_input_base.Image")
+    def test_log_failed_match(self, mock_image, mock_logger):
+        """
+        Test whether the function converts the images to base64
+        and add them to the HTML Robot log.
+        """
+
+        video_input = self.VideoInput()
+        image = Mock()
+        template = mock_image.open.return_value
+
+        video_input._log_failed_match(image, "template")
+
+        mock_image.open.assert_called_with("template")
+        template.convert.assert_called_with("RGB")
+
+        mock_logger.info.assert_called_once()
---
+++ yarf/robot/libraries/zapper/VideoInput.py
@@ -0,0 +1,152 @@
+"""
+This module provides the Zapper-driven implementation for
+video interactions and assertions.
+"""
+
+import os
+from abc import ABC, abstractmethod
+from enum import Enum
+
+import cv2
+from robot.api.deco import library, keyword
+from PIL import Image
+
+
+from yarf.robot.libraries.video_input_base import VideoInputBase
+from yarf.robot.libraries.zapper import zapper_api
+
+
+class Source(Enum):
+    """Available video source devices."""
+
+    HDMI = 0
+    CAMERA = 1
+
+
+class VideoSource(ABC):
+    """Base class for a video source."""
+
+    @abstractmethod
+    def grab_screenshot(self) -> Image.Image:
+        """Grab a screenshot from the video source."""
+
+    @abstractmethod
+    def start_video_input(self):
+        """Stop video input process."""
+
+    @abstractmethod
+    def stop_video_input(self):
+        """Stop video input process."""
+
+
+class HdmiIn(VideoSource):
+    """HDMI-IN as video source."""
+
+    RESOLUTION = "1280x1024"
+    STREAM_QUALITY = 100
+    STREAM_FPS = 10
+
+    USTREAMER_PORT = 60010
+    USTREAMER_SNAPSHOT = "http://{}:{}/snapshot".format(
+        os.getenv("ZAPPER_IP", "localhost"), USTREAMER_PORT
+    )
+
+    def __init__(self):
+        """
+        Open a new HDMI-IN video capture and set properties.
+
+        For this video source, a simple VideoCapture is not enough:
+        there are common situation where the DUT is not displaying anything
+        via HDMI. This makes the B101/v4l block: handling v4l2 at low
+        level is just not possible with OpenCV. uStreamer on the other
+        hand already has a nice implementation of recovering/reloading
+        the device when this happens.
+        """
+        with zapper_api() as service:
+            service.change_hdmi_resolution(self.RESOLUTION)
+        self.start_video_input()
+
+    def stop_video_input(self):
+        """Stop video input process."""
+        with zapper_api() as service:
+            service.hdmi_stream_stop()
+
+    def start_video_input(self):
+        """Start video input process."""
+        with zapper_api() as service:
+            service.hdmi_stream_start(
+                resolution=self.RESOLUTION,
+                quality=self.STREAM_QUALITY,
+                fps=self.STREAM_FPS,
+                restart=True,
+            )
+
+    def grab_screenshot(self) -> Image.Image:
+        """
+        Grab a screenshot from the video source.
+
+        To make it _really_ faster we're pointing to
+        a snapshot HTML page, not the real streaming.
+        Hence, we have to create a new VideoCapture each time.
+        """
+        status, screenshot = cv2.VideoCapture(self.USTREAMER_SNAPSHOT).read()
+        if not status:
+            raise RuntimeError("Cannot grab screenshot.")
+
+        screenshot = cv2.cvtColor(screenshot, cv2.COLOR_BGR2RGB)
+        return Image.fromarray(screenshot)
+
+
+class UsbCam(VideoSource):
+    """USB Camera as video source."""
+
+    def __init__(self):
+        """Open a new USB Cam video capture and set properties."""
+        raise NotImplementedError
+
+    def start_video_input(self):
+        """Start video input process."""
+        raise NotImplementedError
+
+    def stop_video_input(self):
+        """Stop video input process."""
+        raise NotImplementedError
+
+    def grab_screenshot(self) -> Image.Image:
+        """Grab a screenshot from the video source."""
+        raise NotImplementedError
+
+
+@library
+class VideoInput(VideoInputBase):
+    """
+    This class provides access to Zapper video input devices.
+    """
+
+    @keyword
+    def init(self, source_name: str):
+        """Handles platform-specific initialization."""
+        try:
+            source = Source[source_name]
+        except KeyError as exc:
+            raise SystemExit(
+                f"The input source '{source_name}' is not supported."
+            ) from exc
+
+        if source == Source.HDMI:
+            self._source = HdmiIn()
+        else:
+            self._source = UsbCam()
+
+    @keyword
+    def start_video_input(self):
+        """Start video stream process if needed."""
+        self._source.start_video_input()
+
+    @keyword
+    def stop_video_input(self):
+        """Start video stream process if needed."""
+        self._source.stop_video_input()
+
+    def _grab_screenshot(self) -> Image.Image:
+        return self._source.grab_screenshot()
