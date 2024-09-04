d8a3f98e41de0008df0efb58aa721d89df990739
Michał Sawicz <michal@sawicz.net>
2024-09-04 09:55:10 +0200

    lib: add wayland base protocols

---
+++ hatch_build.py
@@ -0,0 +1,25 @@
+from pathlib import Path
+from typing import Dict
+
+from hatchling.builders.hooks.plugin.interface import BuildHookInterface
+
+YARF_PATH = Path(__file__).parent
+INPUT_DIR = YARF_PATH / "data" / "wayland"
+OUTPUT_DIR = YARF_PATH / "yarf" / "lib" / "wayland" / "protocols"
+
+
+def generate_protocol(name: str, imports: Dict[str, str]) -> Dict[str, str]:
+    import pywayland
+    import pywayland.scanner
+
+    proto = pywayland.scanner.Protocol.parse_file(
+        str(INPUT_DIR / f"{name}.xml")
+    )
+    proto_imports = {iface.name: proto.name for iface in proto.interface}
+    proto.output(str(OUTPUT_DIR), dict(proto_imports, **imports))
+    return proto_imports
+
+
+class CustomBuildHook(BuildHookInterface):
+    def initialize(self, version, build_data):
+        generate_protocol("wayland", {})
