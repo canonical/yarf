263880fd4be35828e88252afb0dcf228cb287175
Michał Sawicz <michal.sawicz@canonical.com>
2024-09-17 15:41:18 +0200

    libraries.mir: add mir.VideoInput

---
+++ yarf/rf_libraries/libraries/mir/VideoInput.py
@@ -0,0 +1,54 @@
+"""
+This module provides the Mir-driven implementation for video interactions and
+assertions.
+"""
+
+import asyncio
+import os
+
+from PIL import Image
+from robot.api.deco import keyword, library
+
+from yarf.lib.wayland import screencopy
+from yarf.rf_libraries.libraries.video_input_base import VideoInputBase
+
+
+@library(scope="GLOBAL")
+class VideoInput(VideoInputBase):
+    ROBOT_LISTENER_API_VERSION = 3
+
+    def __init__(self) -> None:
+        self.ROBOT_LIBRARY_LISTENER = self
+        display_name = os.environ.get("WAYLAND_DISPLAY", "wayland-0")
+        self._screencopy = screencopy.Screencopy(display_name)
+        super().__init__()
+
+    async def _grab_screenshot(self) -> Image:
+        """
+        Grabs the current frame through screencopy.
+
+        Returns:
+            Pillow Image of the frame
+        """
+        await self.start_video_input()
+        return await self._screencopy.grab_screenshot()
+
+    @keyword
+    async def start_video_input(self):
+        """
+        Connect to the display.
+        """
+        await self._screencopy.connect()
+
+    @keyword
+    async def stop_video_input(self):
+        """
+        Disconnect from the display.
+        """
+        await self._screencopy.disconnect()
+
+    def _close(self):
+        """
+        Listener method called when the library goes out of scope.
+        """
+        asyncio.get_event_loop().run_until_complete(self.stop_video_input())
