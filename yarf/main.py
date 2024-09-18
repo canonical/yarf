95c9e1039682f660554dc02a7ffdef26ad752885
Paolo Gentili <paolo.gentili@canonical.com>
2024-09-18 14:50:35 +0200

    [New] pass unknown arguments to the Robot CLI parser (#58)
    * Add: pass unknown arguments to the Robot CLI parser
    
    * Add: Robot helper
    
    * Change: updated docs with --robot-help and extra arguments
    
    * Change: using the -- separator for Robot arguments
    
    * Fix: docstrings format
    
    * Fix: docs title
    
    * fix: catch Information robot exception

--- yarf/main.py
+++ yarf/main.py
@@ -1,3 +1,4 @@
+import contextlib
 import logging
 import os
 import shutil
@@ -6,11 +7,13 @@ import tempfile
 from argparse import ArgumentParser, Namespace
 from importlib import metadata
 from pathlib import Path
-from typing import Any
+from typing import Any, Optional
 
 from packaging import version
 from robot import rebot
 from robot.api import TestSuite, TestSuiteBuilder
+from robot.errors import Information
+from robot.run import RobotFramework
 
 from yarf.rf_libraries import robot_in_path
 from yarf.rf_libraries.libraries import SUPPORTED_PLATFORMS, PlatformBase
@@ -20,18 +23,16 @@ _logger = logging.getLogger(__name__)
 YARF_VERSION = version.parse(metadata.version("yarf"))
 
 
-def parse_arguments(argv: list[str] = None) -> Namespace:
+def parse_yarf_arguments(argv: list[str]) -> Namespace:
     """
     Add and parse command line arguments.
 
-    :param argv: list of arguments received via command line, defaults
-        to None
-    :return: the argparse.Namespace got after parsing the input
-    """
+    Args:
+        argv: list of arguments received via command line
 
-    # For easier testing purposes:
-    if argv is None:
-        argv = sys.argv[1:]
+    Returns:
+        The argparse.Namespace got after parsing the input
+    """
 
     top_level_parser = ArgumentParser()
     top_level_parser.add_argument(
@@ -83,6 +84,56 @@ def parse_arguments(argv: list[str] = None) -> Namespace:
     return top_level_parser.parse_args(argv)
 
 
+def parse_robot_arguments(args: list[str]) -> dict[str, Any]:
+    """
+    Parse extra arguments using the Robot CLI argument parser.
+
+    Args:
+        args: list of extra arguments got via CLI
+    Returns:
+        A dictionary of arguments, parsed by the Robot argparse
+    Raises:
+        SystemExit: informational arguments
+    """
+
+    with tempfile.NamedTemporaryFile(suffix=".robot") as stub_robot_file:
+        args.append(stub_robot_file.name)
+        try:
+            options, _ = RobotFramework().parse_arguments(args)
+        except Information as info:
+            print(info)
+            raise SystemExit()
+    return options
+
+
+def parse_arguments(
+    argv: Optional[list[str]] = None,
+) -> tuple[Namespace, dict[str, Any]]:
+    """
+    Parse CLI arguments, treating everything before the "--" separator as a
+    yarf argument, and everything else as robot argument.
+
+    Args:
+        argv: list of arguments from CLI, if None it will be
+            retrieved from `sys`.
+    Returns:
+        The yarf args namespace and the Robot options
+    """
+
+    if argv is None:
+        argv = sys.argv[1:]
+
+    try:
+        separator = argv.index("--")
+        yarf_argv = argv[:separator]
+        robot_argv = argv[separator + 1 :]
+    except ValueError:
+        yarf_argv = argv
+        robot_argv = []
+
+    return parse_yarf_arguments(yarf_argv), parse_robot_arguments(robot_argv)
+
+
 def get_robot_settings(test_suite: TestSuite) -> dict[str, Any]:
     """
     Get yarf settings based on yarf specific tags on each robot task.
@@ -116,21 +167,29 @@ def run_robot_suite(
     lib_cls: PlatformBase,
     variables: list[str],
     outdir: Path,
+    cli_options: dict[str, Any],
 ) -> None:
     """
     Run a robot test suite in the given suite path.
 
-    :param test_suite: an initialized executable TestSuite
-    :param lib_cls: The platform library class that the user has chosen
-        via the option `--platform`
-    :param variables: Variables for the test suite to run
-    :param outdir: Path to the output directory
-    :return: None
+    Args:
+        test_suite: an initialized executable TestSuite
+        lib_cls: The platform library class that the user has chosen
+            via the option `--platform`
+        variables: Variables for the test suite to run
+        outdir: Path to the output directory
+        cli_options: extra options given by CLI
     """
+
     robot_settings = get_robot_settings(test_suite)
+    options = cli_options | robot_settings
+
+    with contextlib.suppress(KeyError):
+        variables.extend(options.pop("variable"))
+
     with robot_in_path(lib_cls.get_pkg_path()):
         result = test_suite.run(
-            variable=variables, outputdir=outdir, **robot_settings
+            variable=variables, outputdir=outdir, **options
         )
 
     # Generate HTML report.html and log.html using rebot().
@@ -146,18 +205,20 @@ def run_interactive_console(
     lib_cls: PlatformBase,
     outdir: Path,
     rf_debug_history_log_path: Path,
+    cli_options: dict[str, Any],
 ) -> None:
     """
     Import the platform libraries and resources, and run the interactive
     console.
 
-    :param test_suite: an initialized executable TestSuite
-    :param lib_cls: The platform library class that the user has chosen
-        via the option `--platform`
-    :param outdir: Path to the output directory
-    :param rf_debug_history_log_path: Path to the interactive console
-        log file
-    :return: None
+    Args:
+        console_suite: an initialized executable TestSuite
+        lib_cls: The platform library class that the user has chosen
+            via the option `--platform`
+        outdir: Path to the output directory
+        rf_debug_history_log_path: Path to the interactive console
+            log file
+        cli_options: extra options given by CLI
     """
     platform_library_paths = []
     for file_path in Path(lib_cls.get_pkg_path()).glob("*.py"):
@@ -179,11 +240,15 @@ def run_interactive_console(
         f"PLATFORM_LIBRARIES:{','.join(platform_library_paths)}",
         f"RESOURCES:{','.join(resources)}",
     ]
+    with contextlib.suppress(KeyError):
+        variables.extend(cli_options.pop("variable"))
+
     with robot_in_path(lib_cls.get_pkg_path()):
         console_suite.run(
             variable=variables,
             outputdir=outdir,
             console="none",
+            **cli_options,
         )
 
     _logger.info(
@@ -193,15 +258,17 @@ def run_interactive_console(
     rebot(f"{outdir}/output.xml", outputdir=outdir)
 
 
-def main(argv: list[str] = None) -> None:
+def main(argv: Optional[list[str]] = None) -> None:
     """
     Main entry point.
 
-    :param argv: list of arguments received via command line, defaults
-        to None
-    :return: None
+    Args:
+        argv: list of arguments received via command line, defaults
+            to None
     """
-    args = parse_arguments(argv)
+
+    args, cli_options = parse_arguments(argv)
+
     lib_cls = SUPPORTED_PLATFORMS.get(args.platform)
     logging.basicConfig(level=args.verbosity)
     outdir = Path(
@@ -219,7 +286,9 @@ def main(argv: list[str] = None) -> None:
         ) as temp_folder_path:
             test_suite = TestSuite.from_file_system(temp_folder_path)
             test_suite.name = suite_parser.suite_path.absolute().name
-            run_robot_suite(test_suite, lib_cls, variables, outdir)
+            run_robot_suite(
+                test_suite, lib_cls, variables, outdir, cli_options
+            )
 
         _logger.info(f"Results exported to: {outdir}")
 
@@ -240,7 +309,11 @@ def main(argv: list[str] = None) -> None:
         console_suite = TestSuiteBuilder().build(start_console_path)
         console_suite.name = f"{lib_cls.__name__} Interactive Console"
         run_interactive_console(
-            console_suite, lib_cls, outdir, Path(os.environ["RFDEBUG_HISTORY"])
+            console_suite,
+            lib_cls,
+            outdir,
+            Path(os.environ["RFDEBUG_HISTORY"]),
+            cli_options,
         )
 
 
