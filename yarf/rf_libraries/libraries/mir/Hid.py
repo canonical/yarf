7b2a6042a5b938643c1c5f11134f4bfd8ea9fc6f
Michał Sawicz <michal.sawicz@canonical.com>
2024-11-07 16:02:30 +0100

    mir: add docstrings

--- yarf/rf_libraries/libraries/mir/Hid.py
+++ yarf/rf_libraries/libraries/mir/Hid.py
@@ -1,5 +1,6 @@
 import asyncio
 import os
+from typing import Any
 
 from robot.api.deco import keyword, library
 
@@ -27,7 +28,13 @@ class Hid(HidBase):
         self._connected: bool = False
         super().__init__()
 
-    def _start_test(self, *args):
+    def _start_test(self, *_args: Any):
+        """
+        Listener method called when tests are started.
+
+        Arguments:
+            *_args: unused
+        """
         if not self._connected:
             asyncio.get_event_loop().run_until_complete(self._connect())
 
@@ -46,23 +53,51 @@ class Hid(HidBase):
         )
 
     async def _move_pointer(self, x: float, y: float) -> None:
+        """
+        Move the pointer to proportional (x, y) position.
+
+        Arguments:
+            x: horizontal coordinate, 0 <= x <= 1
+            y: vertical coordinate, 0 <= y <= 1
+        """
         self._virtual_pointer.move_to_proportional(x, y)
 
     @keyword
     async def press_pointer_button(self, button: str) -> None:
+        """
+        Press pointer button.
+
+        Arguments:
+            button: one of LEFT, MIDDLE, RIGHT
+        """
         self._virtual_pointer.button(Button[button], True)
 
     @keyword
     async def release_pointer_button(self, button: str) -> None:
+        """
+        Release pointer button.
+
+        Arguments:
+            button: one of LEFT, MIDDLE, RIGHT
+        """
         self._virtual_pointer.button(Button[button], False)
 
     @keyword
     async def click_pointer_button(self, button: str) -> None:
+        """
+        Click pointer button.
+
+        Arguments:
+            button: one of LEFT, MIDDLE, RIGHT
+        """
         self._virtual_pointer.button(Button[button], True)
         self._virtual_pointer.button(Button[button], False)
 
     @keyword
     async def release_pointer_buttons(self) -> None:
+        """
+        Release all pointer buttons.
+        """
         for button in Button:
             self._virtual_pointer.button(button, False)
 
