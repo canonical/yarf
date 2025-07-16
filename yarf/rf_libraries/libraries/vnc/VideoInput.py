1b9e04fc6a50e73d6726d4a88844f0ccb016636b
Tim Andersson <129757484+andersson1234@users.noreply.github.com>
2025-07-16 17:34:51 +0100

    BugFix: Fix vnc platform hanging indefinitely (#236)

--- yarf/rf_libraries/libraries/vnc/VideoInput.py
+++ yarf/rf_libraries/libraries/vnc/VideoInput.py
@@ -1,5 +1,9 @@
+from asyncio import TimeoutError, wait_for
+from time import sleep
+
 from asyncvnc import connect
 from PIL import Image
+from robot.api import logger
 from robot.api.deco import keyword, library
 
 from yarf.rf_libraries.libraries.video_input_base import VideoInputBase
@@ -10,7 +14,16 @@ from yarf.rf_libraries.libraries.vnc import Vnc
 class VideoInput(VideoInputBase):
     """
     This class provides access to screenshotting via vnc.
+
+    Attributes:
+        screenshot_timeout: The time to wait for client.screenshot() to return
+        screenshot_retries: The amount of times to retry getting a screenshot
+        screenshot_sleep_interval: The sleep between retries when trying to get a screenshot
     """
+
+    screenshot_timeout = 10
+    screenshot_retries = 6
+    screenshot_sleep_interval = 1
 
     def __init__(self) -> None:
         super().__init__()
@@ -19,9 +32,24 @@ class VideoInput(VideoInputBase):
     @keyword
     async def grab_screenshot(self) -> Image.Image:
         screenshot = None
-        async with connect(self.vnc.host, self.vnc.port) as client:
-            screenshot = Image.fromarray(await client.screenshot())
-        return screenshot
+        for attempt in range(self.screenshot_retries):
+            async with connect(self.vnc.host, self.vnc.port) as client:
+                try:
+                    screenshot = Image.fromarray(
+                        await wait_for(
+                            client.screenshot(), self.screenshot_timeout
+                        )
+                    )
+                except TimeoutError:
+                    logger.info(
+                        f"attempt {attempt + 1}/{self.screenshot_retries}: asyncvnc timed out after {self.screenshot_timeout} seconds, sleeping for {self.screenshot_sleep_interval} seconds and trying again..."
+                    )
+                    sleep(self.screenshot_sleep_interval)
+            if screenshot:
+                return screenshot
+        raise TimeoutError(
+            "Failed to get screenshots via asyncvnc, something went wrong!"
+        )
 
     @keyword
     async def stop_video_input(self) -> None:
--- yarf/rf_libraries/libraries/vnc/tests/test_video_input.py
+++ yarf/rf_libraries/libraries/vnc/tests/test_video_input.py
@@ -35,6 +35,28 @@ class TestVncVideoInput:
                 client_mock.screenshot.assert_called_once
 
     @pytest.mark.asyncio
+    async def test_grab_screenshot_timeout(
+        self,
+        monkeypatch,
+        mock_image,
+    ):
+        with monkeypatch.context() as m:
+            m.setenv("VNC_PORT", "1")
+            m.setenv("VNC_HOST", "localhost")
+            video_input = VideoInput()
+            screenshot = None
+            with patch(
+                "yarf.rf_libraries.libraries.vnc.VideoInput.connect",
+                new=MagicMock(),
+            ) as connect_mock:
+                client_mock = connect_mock.return_value.__aenter__.return_value
+                client_mock.screenshot = AsyncMock()
+                client_mock.screenshot.side_effect = TimeoutError
+                with pytest.raises(TimeoutError):
+                    screenshot = await video_input.grab_screenshot()
+            assert screenshot is None
+
+    @pytest.mark.asyncio
     async def test_private_grab_screenshot(
         self,
         monkeypatch,
