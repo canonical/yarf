761edbb010f0b077f0d8dbaf03434ffe218a92c7
Douglas Chiang <douglas.chiang@canonical.com>
2025-08-28 17:07:13 +0100

    Change pointer functions library name because they clash with kvm ones
    Signed-off-by: Douglas Chiang <douglas.chiang@canonical.com>

--- yarf/rf_libraries/libraries/hid_base.py
+++ yarf/rf_libraries/libraries/hid_base.py
@@ -63,7 +63,7 @@ class HidBase(ABC):
 
     @abstractmethod
     @keyword
-    async def press_pointer_button(self, button: str) -> None:
+    async def pointer_press_button(self, button: str) -> None:
         """
         Press the specified pointer button.
 
@@ -73,7 +73,7 @@ class HidBase(ABC):
 
     @abstractmethod
     @keyword
-    async def release_pointer_button(self, button: str) -> None:
+    async def pointer_release_button(self, button: str) -> None:
         """
         Release the specified pointer button.
 
@@ -83,7 +83,7 @@ class HidBase(ABC):
 
     @abstractmethod
     @keyword
-    async def click_pointer_button(self, button: str) -> None:
+    async def pointer_click_button(self, button: str) -> None:
         """
         Press and release the specified pointer button.
 
@@ -93,7 +93,7 @@ class HidBase(ABC):
 
     @abstractmethod
     @keyword
-    async def release_pointer_buttons(self) -> None:
+    async def pointer_release_buttons(self) -> None:
         """
         Release all pointer buttons.
         """
@@ -117,7 +117,7 @@ class HidBase(ABC):
         """
 
     @abstractmethod
-    async def _move_pointer(self, x: float, y: float) -> None:
+    async def _pointer_move(self, x: float, y: float) -> None:
         """
         Platform implementation of the pointer move.
 
@@ -127,7 +127,7 @@ class HidBase(ABC):
         """
 
     @keyword
-    async def move_pointer_to_proportional(self, x: float, y: float) -> None:
+    async def pointer_move_to_proportional(self, x: float, y: float) -> None:
         """
         Move the virtual pointer to a position proportional to the size of the
         output.
@@ -143,11 +143,11 @@ class HidBase(ABC):
         assert 0 <= x <= 1, "x not in range 0..1"
         assert 0 <= y <= 1, "y not in range 0..1"
 
-        await self._move_pointer(x, y)
+        await self._pointer_move(x, y)
         self._pointer_position = PointerPosition(x, y)
 
     @keyword
-    async def move_pointer_to_absolute(self, x: int, y: int) -> None:
+    async def pointer_move_to_absolute(self, x: int, y: int) -> None:
         """
         Move the virtual pointer to an absolute position within the output.
 
@@ -168,11 +168,11 @@ class HidBase(ABC):
         assert 0 <= y <= display_size.height, "Y coordinate outside of screen"
 
         proportional = (x / display_size.width, y / display_size.height)
-        await self._move_pointer(*proportional)
+        await self._pointer_move(*proportional)
         self._pointer_position = PointerPosition(*proportional)
 
     @keyword
-    async def walk_pointer_to_absolute(
+    async def pointer_walk_to_absolute(
         self,
         x: int,
         y: int,
@@ -202,24 +202,15 @@ class HidBase(ABC):
         assert 0 <= y <= display_size.height, "Y coordinate outside of screen"
 
         proportional = (x / display_size.width, y / display_size.height)
-        proportional_step_distance = (
-            step_distance / display_size.width,
-            step_distance / display_size.height,
-        )
-        await self.walk_pointer_to_proportional(
+        await self.pointer_walk_to_proportional(
             *proportional,
             *proportional_step_distance,
             delay,
         )
 
     @keyword
-    async def walk_pointer_to_proportional(
-        self,
-        x: float,
-        y: float,
-        step_distance_x: float,
-        step_distance_y: float,
-        delay: float,
+    async def pointer_walk_to_proportional(
+        self, x: float, y: float, step_distance: float, delay: float
     ) -> None:
         """
         Walk the virtual pointer to a position proportional to the size of the
@@ -251,7 +242,7 @@ class HidBase(ABC):
                 dist_y < 0 and -1 or 1
             )
 
-            await self.move_pointer_to_proportional(
+            await self.pointer_move_to_proportional(
                 self._pointer_position.x + step_x,
                 self._pointer_position.y + step_y,
             )
--- yarf/rf_libraries/libraries/tests/test_hid_base.py
+++ yarf/rf_libraries/libraries/tests/test_hid_base.py
@@ -12,7 +12,7 @@ def mock_sleep():
 
 
 class StubHid(HidBase):
-    async def click_pointer_button(self, *args):
+    async def pointer_click_button(self, *args):
         pass
 
     async def _keys_combo(self, *args):
@@ -24,16 +24,16 @@ class StubHid(HidBase):
     async def _get_display_size(self):
         pass
 
-    async def _move_pointer(self, *args):
+    async def _pointer_move(self, *args):
         pass
 
-    async def press_pointer_button(self, *args):
+    async def pointer_press_button(self, *args):
         pass
 
-    async def release_pointer_button(self, *args):
+    async def pointer_release_button(self, *args):
         pass
 
-    async def release_pointer_buttons(self, *args):
+    async def pointer_release_buttons(self, *args):
         pass
 
 
@@ -41,64 +41,64 @@ class StubHid(HidBase):
 def stub_hid():
     ph = StubHid()
     ph._get_display_size = AsyncMock(return_value=Size(1000, 1000))
-    ph._move_pointer = AsyncMock()
+    ph._pointer_move = AsyncMock()
     ph._keys_combo = AsyncMock()
     yield ph
 
 
 class TestHidBase:
     @pytest.mark.asyncio
-    async def test_move_pointer_to_absolute(self, stub_hid):
+    async def test_pointer_move_to_absolute(self, stub_hid):
         """
         Test the mouse movement processing.
         """
-        await stub_hid.move_pointer_to_absolute(100, 200)
-        stub_hid._move_pointer.assert_called_with(
+        await stub_hid.pointer_move_to_absolute(100, 200)
+        stub_hid._pointer_move.assert_called_with(
             100 / 1000,
             200 / 1000,
         )
 
     @pytest.mark.asyncio
-    async def test_move_pointer_to_absolute_raises(self, stub_hid):
+    async def test_pointer_move_to_absolute_raises(self, stub_hid):
         """
         Test the mouse movement processing.
         """
         with pytest.raises(AssertionError):
-            await stub_hid.move_pointer_to_absolute(1001, 0)
+            await stub_hid.pointer_move_to_absolute(1001, 0)
 
         with pytest.raises(AssertionError):
-            await stub_hid.move_pointer_to_absolute(0, 1001)
+            await stub_hid.pointer_move_to_absolute(0, 1001)
 
     @pytest.mark.asyncio
-    async def test_move_pointer_to_proportional(self, stub_hid):
+    async def test_pointer_move_to_proportional(self, stub_hid):
         """
         Test the proportional mouse movement processing.
         """
-        await stub_hid.move_pointer_to_proportional(0.5, 0.5)
-        stub_hid._move_pointer.assert_called_with(0.5, 0.5)
+        await stub_hid.pointer_move_to_proportional(0.5, 0.5)
+        stub_hid._pointer_move.assert_called_with(0.5, 0.5)
 
     @pytest.mark.asyncio
-    async def test_move_pointer_to_proportional_raises(self, stub_hid):
+    async def test_pointer_move_to_proportional_raises(self, stub_hid):
         """
         Test the function raises an exception if the target position is out of
         screen.
         """
         with pytest.raises(AssertionError):
-            await stub_hid.move_pointer_to_proportional(1.1, 0)
+            await stub_hid.pointer_move_to_proportional(1.1, 0)
 
         with pytest.raises(AssertionError):
-            await stub_hid.move_pointer_to_proportional(0, 1.1)
+            await stub_hid.pointer_move_to_proportional(0, 1.1)
 
     @pytest.mark.asyncio
-    async def test_walk_pointer_to_proportional(self, stub_hid, mock_sleep):
+    async def test_pointer_walk_to_proportional(self, stub_hid, mock_sleep):
         """
         Test the function moves the pointer by the requested step to the target
         position given in proportional coordinates.
         """
-        await stub_hid.move_pointer_to_proportional(0.15, 0.35)
-        await stub_hid.walk_pointer_to_proportional(0.5, 0.5, 0.05, 0.05, 0.2)
+        await stub_hid.pointer_move_to_proportional(0.15, 0.35)
+        await stub_hid.pointer_walk_to_proportional(0.5, 0.5, 0.05, 0.2)
 
-        stub_hid._move_pointer.assert_has_calls(
+        stub_hid._pointer_move.assert_has_calls(
             (
                 call(pytest.approx(0.2), pytest.approx(0.4)),
                 call(pytest.approx(0.25), pytest.approx(0.45)),
@@ -113,42 +113,15 @@ class TestHidBase:
         mock_sleep.assert_has_calls(7 * [call(0.2)])
 
     @pytest.mark.asyncio
-    @pytest.mark.parametrize(
-        "x,y,step_distance_x,step_distance_y",
-        [
-            (1.1, 0, 0, 0),
-            (0, 1.1, 0, 0),
-            (0, 0, 1.1, 0),
-            (0, 0, 0, 1.1),
-        ],
-    )
-    async def test_walk_pointer_to_proportional_raises(
-        self,
-        stub_hid: MagicMock,
-        x: float,
-        y: float,
-        step_distance_x: float,
-        step_distance_y: float,
-    ):
-        """
-        Test the function raises an exception if the target position or step
-        distance are out of range.
-        """
-        with pytest.raises(AssertionError):
-            await stub_hid.walk_pointer_to_proportional(
-                x, y, step_distance_x, step_distance_y, 0.2
-            )
-
-    @pytest.mark.asyncio
-    async def test_walk_pointer_to_absolute(self, stub_hid, mock_sleep):
+    async def test_pointer_walk_to_absolute(self, stub_hid, mock_sleep):
         """
         Test the function moves the pointer by the requested step to the target
         position given in absolute coordinates.
         """
-        await stub_hid.move_pointer_to_proportional(0.15, 0.35)
-        await stub_hid.walk_pointer_to_absolute(100, 200, 100, 0.2)
+        await stub_hid.pointer_move_to_proportional(0.15, 0.35)
+        await stub_hid.pointer_walk_to_absolute(100, 200, 0.05, 0.2)
 
-        stub_hid._move_pointer.assert_has_calls(
+        stub_hid._pointer_move.assert_has_calls(
             (
                 call(pytest.approx(0.1), pytest.approx(0.25)),
                 call(pytest.approx(0.1), pytest.approx(0.2)),
