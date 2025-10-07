112f9e31c558f8d0235d5e2efdb57b7d6b7e8785
Douglas Chiang <douglas.chiang@canonical.com>
2025-10-07 12:54:52 +0100

    Move preparing cmds to scripts
    Signed-off-by: Douglas Chiang <douglas.chiang@canonical.com>

---
+++ .github/scripts/canary-test/prepare-platform.sh
@@ -0,0 +1,13 @@
+# Start Mir on a virtual display (doesn't require graphics hardware)
+mir-test-tools.demo-server \
+    --add-wayland-extensions zwlr_screencopy_manager_v1:zwlr_virtual_pointer_manager_v1 \
+    --platform-display-libs mir:virtual \
+    --virtual-output 1280x1024 &
+
+# Wait for the compositor to start
+inotifywait --event create --include "^$XDG_RUNTIME_DIR/$WAYLAND_DISPLAY\$" $XDG_RUNTIME_DIR
+
+if [[ "$PLATFORM" == "Vnc" ]]; then
+    # Start vnc session
+    wayvnc &
+fi
