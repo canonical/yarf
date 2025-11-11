ad0d93b5525c5a405d4a47a2a9b00c91abb45495
Adrian Dombeck <adrian.dombeck@canonical.com>
2025-11-11 13:25:31 +0100

    [New] Consistent usage of "similarity" and "confidence"
    We used the terms "confidence" and "coincidence", which are ambiguous
    and used inconsistently:
    
    * When using tesseract, only "confidence" is used, and it refers to the
      similarity between the text that it tries to find and the text that
      was returned by OCR.
    
    * When using RapidOCR:
      * "confidence" refers to the confidence score returned by RapidOCR,
        which is the model's estimated probability that the returned text is
        actually what is visible in the image.
      * "coincidence" refers to the same which "confidence" refers to in
        tesseract: the similarity between the text that we try to find and
        the text that was returned by OCR
    
    * In the matches dictionaries returned by find_text, the confidence item
      always refers to the text similarity, even when using RapidOCR, where
      the coincidence argument is used for that.
    
    * For RapidOCR, the "confidence" value is a fraction (between 0 and 1),
      for the other values we use percentage (between 0 and 100).
    
    We now consistently use:
    * "similarity" for text similarity
    * "confidence" for the OCR confidence score
    * Percentage (0 to 100) for all similarity and confidence values.

--- yarf/rf_libraries/libraries/ocr/rapidocr.py
+++ yarf/rf_libraries/libraries/ocr/rapidocr.py
@@ -24,7 +24,7 @@ class OCRResult:
     Attributes:
         position: Quadrilateral region of the match.
         text: Text found in the match.
-        confidence: Confidence of the match
+        confidence: Estimated probability that the recognized text is correct.
     """
 
     position: Quad
@@ -44,12 +44,16 @@ class RapidOCRReader:
     This class is a singleton to avoid loading the model multiple times.
 
     Attributes:
-        DEFAULT_CONFIDENCE: Default confidence for text detection.
-        DEFAULT_COINCIDENCE: Default coincidence for text similarities.
+        DEFAULT_SIMILARITY_THRESHOLD: Minimum similarity percentage (0-100) for
+         text matching. If the similarity between the found text and the target
+         text is below this threshold, the match is discarded.
+        DEFAULT_CONFIDENCE_THRESHOLD: Minumum confidence percentage (0-100) for
+          text matching. If the confidence of the found text is below this
+          threshold, the match is discarded.
     """
 
-    DEFAULT_CONFIDENCE: float = 0.7
-    DEFAULT_COINCIDENCE: float = 80.0
+    DEFAULT_SIMILARITY_THRESHOLD: float = 80.0
+    DEFAULT_CONFIDENCE_THRESHOLD: float = 70.0
 
     def __new__(cls) -> "RapidOCRReader":
         if not hasattr(cls, "instance"):
@@ -82,8 +86,8 @@ class RapidOCRReader:
         self,
         image: Image.Image | Path,
         text: str,
-        confidence: float = DEFAULT_CONFIDENCE,
-        coincidence: float = DEFAULT_COINCIDENCE,
+        similarity_threshold: float = DEFAULT_SIMILARITY_THRESHOLD,
+        confidence_threshold: float = DEFAULT_CONFIDENCE_THRESHOLD,
         region: Region | None = None,
         partial: bool = True,
     ) -> list[dict]:
@@ -94,8 +98,12 @@ class RapidOCRReader:
         Args:
             image: Path to image or Image object.
             text: Text to find in image.
-            confidence: Minimum confidence for text detection.
-            coincidence: Minimum coincidence for text similarities.
+            similarity_threshold: Minimum similarity percentage (0-100) for
+              text matching. If the similarity between the found text and the
+              target text is below this threshold, the match is discarded.
+            confidence_threshold: Minimum confidence percentage (0-100) for
+              text matching. If the confidence of the found text is below this
+              threshold, the match is discarded.
             region: Limit the region of the screen where to look.
             partial: Use partial matching.
 
@@ -123,9 +131,12 @@ class RapidOCRReader:
                 ocr_output.boxes, ocr_output.txts, ocr_output.scores
             )
         ]
+        # Multiply the item confidence with 100 to convert it to percentage
+        for item in result:
+            item.confidence *= 100
 
         matches = self.get_matches(
-            result, text, confidence, coincidence, partial
+            result, text, similarity_threshold, confidence_threshold, partial
         )
 
         if region is not None:
@@ -138,8 +149,8 @@ class RapidOCRReader:
         self,
         result: list[OCRResult],
         match_text: str,
-        confidence: float,
-        coincidence: float,
+        similarity_threshold: float,
+        confidence_threshold: float,
         partial: bool,
     ) -> list[dict]:
         """
@@ -148,8 +159,12 @@ class RapidOCRReader:
         Args:
             result: List with the OCR results.
             match_text: Text to match.
-            confidence: Minimum confidence for text detection.
-            coincidence: Minimum coincidence for text similarities.
+            similarity_threshold: Minimum similarity percentage (0-100) for
+              text matching. If the similarity between the found text and the
+              target text is below this threshold, the match is discarded.
+            confidence_threshold: Minimum confidence percentage (0-100) for
+              text matching. If the confidence of the found text is below this
+              threshold, the match is discarded.
             partial: Use partial matching.
 
         Returns:
@@ -187,17 +202,21 @@ class RapidOCRReader:
 
         matches = []
         for item in result:
-            ratio = (
+            similarity = (
                 directional_ratio(match_text, item.text)
                 if partial
                 else rapidfuzz.fuzz.ratio(item.text, match_text)
             )
-            if ratio >= coincidence and item.confidence >= confidence:
+            if (
+                similarity >= similarity_threshold
+                and item.confidence >= confidence_threshold
+            ):
                 matches.append(
                     {
                         "text": item.text,
                         "region": item.position.to_region(),
-                        "confidence": ratio,  # Using the ratio like tesseract
+                        "similarity": similarity,
+                        "confidence": item.confidence,
                     }
                 )
-        return sorted(matches, key=lambda x: x["confidence"], reverse=True)
+        return sorted(matches, key=lambda x: x["similarity"], reverse=True)
