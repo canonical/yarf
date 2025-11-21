9ff7db3c02cfc1b1f8d048e7a65d0e2096d6e86e
Douglas Chiang <douglas.chiang@canonical.com>
2025-11-21 22:39:52 +0000

    use tabulate
    Signed-off-by: Douglas Chiang <douglas.chiang@canonical.com>

--- .github/scripts/yarf/check_unused_keywords.py
+++ .github/scripts/yarf/check_unused_keywords.py
@@ -6,6 +6,8 @@ import json
 import sys
 from pathlib import Path
 
+from tabulate import tabulate
+
 UNUSED_FILE_PATH = Path("unused_keywords.json")
 
 if not UNUSED_FILE_PATH.exists():
@@ -20,14 +22,17 @@ if not data:
     print("🎉 All keywords are used!")
     sys.exit(0)
 
-print("## ❗ Unused Robot Framework Keywords\n")
-print("| Keyword | Source | Type | Class |")
-print("|---------|---------|------|--------|")
-for kw_name, info in data.items():
-    source = info.get("source", "")
-    typ = info.get("type", "")
-    cls = info.get("class", "")
-
-    print(f"| {kw_name} | {source} | {typ} | {cls} |")
+print("❗ Unused Robot Framework Keywords\n")
+table = [
+    [kw, info["source"], info["type"], info["class"]]
+    for kw, info in data.items()
+]
+print(
+    tabulate(
+        table,
+        headers=["Keyword", "Source", "Type", "Class"],
+        tablefmt="github",
+    )
+)
 
 sys.exit(1)
