af2b5ff64322687b5fe5c0b86b3cda0040510dd5
Douglas Chiang <douglasdothnc@gmail.com>
2026-02-02 18:55:26 +0000

    Amend missing changes
    Signed-off-by: Douglas Chiang <douglasdothnc@gmail.com>

--- yarf/rf_libraries/libraries/mir/VideoInput.py
+++ yarf/rf_libraries/libraries/mir/VideoInput.py
@@ -11,7 +11,7 @@ from PIL import Image
 from robot.api.deco import keyword, library
 
 from yarf.lib.wayland import screencopy
-from yarf.logging.owasp_logger import get_owasp_logger
+from yarf.loggings.owasp_logger import get_owasp_logger
 from yarf.rf_libraries.libraries.video_input_base import VideoInputBase
 
 _owasp_logger = OWASPLogger(appid=__name__, logger=get_owasp_logger())
